f9dca83daeb37e8964c97d2ebe7f245aabb8cec5
Pranav Kant <pranav913@gmail.com>
2015-01-26 22:47:14 +0530

    minor corrections to fib.c

--- fib.c
+++ fib.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-
 int fib (int n){
-   int a =0, b =1,i;
-   int c;
+   int a = 0;
+   int b = 1;
+   int i =0, c = 0;
    for (i = 0; i < n; i++){
       c = a + b;
       a = b;
