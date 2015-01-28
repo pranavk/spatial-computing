8ccdd357ce3cfcb95358abf14c92014391b5b2b6
Pranav Kant <pranav913@gmail.com>
2015-01-28 14:33:48 +0530

    Use proper library name

--- annotateWaves.sh
+++ annotateWaves.sh
@@ -1,4 +1,4 @@
 # Helper Script to run wave annotation.
 # First argument should be the file in bitcode format (.bc)
 
-gmake && opt -load ../../../Release+Asserts/lib/Waves.so -annotate < $1
+gmake && opt -load ../../../Release+Asserts/lib/Spatial.so -annotateWaves < $1
