d827d245a55b29c2807bbb7a472bfd87b9d7dcf2
Pranav Kant <pranav913@gmail.com>
2015-02-09 14:36:17 +0530

    removed helper scripts
    Let's keep the source directory clean. Current scripts doesn't contain
    much of the stuff, hence removing them for now.

--- annotateWaves.sh
+++
@@ -1,4 +0,0 @@
-# Helper Script to run wave annotation.
-# First argument should be the file in bitcode format (.bc)
-
-gmake && opt -load ../../../Release+Asserts/lib/Spatial.so -annotateWaves < $1
--- dotToImage.sh
+++
@@ -1,4 +0,0 @@
-# Helper script to write dot format file in form of image.
-# First argument should be the name of the dot file.
-
-dot -Tpng $1 > $1.png
