pranavk/spatial-computing

commit ce76ba1a5bace61d82e90a7cf93e1e2452e6a97e

added switch(es), gamma and beta nodes

pdw/base_node.hpp
@@ -15,6 +15,7 @@ class BaseNode
   int out_edges;
   std::string name;
 public:
+  BaseNode(int in_edges, int out_edges, std::string name) : in_edges(in_edges), out_edges(out_edges), name(name) {}
   int get_no_of_in_edges ();
   int get_no_of_out_edges ();
   int get_name ();

pdw/beta.hpp
@@ -1,0 +1,21 @@
+/*
+ * Spatial Computation - Manifestation of Spatial computing.
+ * Copyright © 2015 Shashwat Pandey
+ *
+ * This code is available to you under Apache License Version 2.0, January
+ * 2014. You can grab a copy of license from the same repository from where you
+ * fetched this code.
+ */
+
+#include "base_node.hpp"
+
+class Beta : public BaseNode {
+	int vinit, vnext;
+	bool P, first;
+	int out;
+
+	void *P, *vinit, *vnext;
+	void *out;
+
+	Beta() : BaseNode(3, 1, "beta") { first = TRUE;} 
+}

pdw/gamma.hpp
@@ -1,0 +1,21 @@
+/*
+ * Spatial Computation - Manifestation of Spatial computing.
+ * Copyright © 2015 Shashwat Pandey
+ *
+ * This code is available to you under Apache License Version 2.0, January
+ * 2014. You can grab a copy of license from the same repository from where you
+ * fetched this code.
+ */
+
+#include "base_node.hpp"
+
+class Gamma : public BaseNode {
+	bool P;
+	int vtrue, vfalse;
+	int out;
+
+	void *P, *vtrue, *vfalse;
+	void *out;
+
+	Gamma() : BaseNode(3, 1, "gamma") {} 
+}

pdw/switch_f.hpp
@@ -1,0 +1,21 @@
+/*
+ * Spatial Computation - Manifestation of Spatial computing.
+ * Copyright © 2015 Shashwat Pandey
+ *
+ * This code is available to you under Apache License Version 2.0, January
+ * 2014. You can grab a copy of license from the same repository from where you
+ * fetched this code.
+ */
+
+#include "base_node.hpp"
+
+class SwitchFalse : public BaseNode {
+	bool P;
+	int v;
+	int out;
+
+	void *P, *v;
+	void *out;
+
+	SwitchFalse() : BaseNode(2, 1, "switchfalse") {} 
+}

pdw/switch_t.hpp
@@ -1,0 +1,21 @@
+/*
+ * Spatial Computation - Manifestation of Spatial computing.
+ * Copyright © 2015 Shashwat Pandey
+ *
+ * This code is available to you under Apache License Version 2.0, January
+ * 2014. You can grab a copy of license from the same repository from where you
+ * fetched this code.
+ */
+
+#include "base_node.hpp"
+
+class SwitchTrue : public BaseNode {
+	bool P;
+	int v;
+	int out;
+
+	void *P, *v;
+	void *out;
+
+	SwitchTrue() : BaseNode(2, 1, "switchtrue") {} 
+}

commit d52c64e6c5083f42ebfbcaf1548ede95829936b9

corrected few syntax errors in class structure
Fallout from ce76ba1a5bace61d82e90a7cf93e1e2452e6a97e.

pdw/base_node.hpp
@@ -18,5 +18,5 @@ public:
   BaseNode(int in_edges, int out_edges, std::string name) : in_edges(in_edges), out_edges(out_edges), name(name) {}
   int get_no_of_in_edges ();
   int get_no_of_out_edges ();
-  int get_name ();
+  std::string get_name ();
 };

pdw/beta.hpp
@@ -14,8 +14,8 @@ class Beta : public BaseNode {
 	bool P, first;
 	int out;
 
-	void *P, *vinit, *vnext;
-	void *out;
+	void *P_ptr, *vinit_ptr, *vnext_ptr;
+	void *out_ptr;
 
-	Beta() : BaseNode(3, 1, "beta") { first = TRUE;} 
-}
+	Beta() : BaseNode(3, 1, "beta") { first = true;}
+};

pdw/gamma.hpp
@@ -14,8 +14,8 @@ class Gamma : public BaseNode {
 	int vtrue, vfalse;
 	int out;
 
-	void *P, *vtrue, *vfalse;
-	void *out;
+	void *P_ptr, *vtrue_ptr, *vfalse_ptr;
+	void *out_ptr;
 
 	Gamma() : BaseNode(3, 1, "gamma") {} 
-}
+};

pdw/switch_f.hpp
@@ -14,8 +14,8 @@ class SwitchFalse : public BaseNode {
 	int v;
 	int out;
 
-	void *P, *v;
-	void *out;
+	void *P_ptr, *v_ptr;
+	void *out_ptr;
 
 	SwitchFalse() : BaseNode(2, 1, "switchfalse") {} 
-}
+};

pdw/switch_t.hpp
@@ -14,8 +14,8 @@ class SwitchTrue : public BaseNode {
 	int v;
 	int out;
 
-	void *P, *v;
-	void *out;
+	void *P_ptr, *v_ptr;
+	void *out_ptr;
 
 	SwitchTrue() : BaseNode(2, 1, "switchtrue") {} 
-}
+};